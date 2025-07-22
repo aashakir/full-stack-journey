a10bcc07d1cfd6948a2d266da5f505584d6b328c
Akhlaq Ahmad <aashakir484@gmail.com>
2025-07-22 12:17:40 +0500

    Day 33: Added skills progress bar

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("contactForm");
   const dynamicContent = document.getElementById("dynamicContent");
